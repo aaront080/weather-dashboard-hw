4bc15516e96a5d7539ad7c290f0c53717bc28daf
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-10 22:56:55 -0700

    click event listener and local storage added

--- script.js
+++ script.js
@@ -1,93 +1,116 @@
 var APIkey = "2bfcbaaa0f6a0ef229a56e34395f8102";
-var date = dayjs().format('L');
+var currentDate = dayjs().format('DD/MM/YYYY');
 var searchHistory = [];
 
-/*var cityNameInput = $('#cityName')
-var searchBtn = $('#searchBtn')
-var searchHistory = $('#pastSearch')
-
-var currentCity
-
-function cityWeather(data) {
-     var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}'
-
-     fetch(requestURL)
-     .then(function(apiResponse) {
-          return apiResponse.json();
-     })
-     .then(function(data) {
-          var cityWeatherEl = $('#cityWeather');
-
-          var cityName = $('<h3>');
-          cityWeather.append(cityName);
-     })
-}*/
 
 
-
-function currentWeather(city) {
-     var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`;
+function currentWeather(search) {
+     var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${APIkey}&units=imperial`;
      $.ajax({
           url: queryUrl,
           method: "GET"
      }).then(function(cityWeather) {
           console.log(cityWeather);
 
-          $("#cityCard").css("display", "block");
-          $("#cityWeather").empty();
-
-          var icon = cityWeather.weather[0].icon;
-          var iconLink = `https://openweather.org/img/wn/${icon}.png`
+        
+          var iconLink = `https://openweather.org/img/wn/${cityWeather.weather[0].icon}@2x.png`;
 
           var searchCity = $(`
-               <h3>${cityWeather.name}<h3>
-               <p>${date} <img src="${iconLink}/></p>
-               <p>Temp: ${cityWeather.main.temp}</p>
-               <p>Wind: ${cityWeather.wind.speed}</p>
-               <p>Humidity: ${cityWeather.main.humidity}</p>               
+               <h3>${cityWeather.name}<span></h3>
+               <img src="${iconLink}" />           
+               <p>${currentDate}</p>
+               <p>Temp: ${cityWeather.main.temp} F</p>
+               <p>Wind: ${cityWeather.wind.speed} mph</p>
+               <p>Humidity: ${cityWeather.main.humidity}%</p>               
                `);
                
                $("#cityWeather").append(searchCity)
+
+               var lat = cityWeather.city.coord.lat;
+               var lon = cityWeather.city.coord.lon;
      })
 }
 
 function fiveDayForecast(lat, lon) {
+
      var forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={APIkey}&units=imperial`
-     $.ajax9({
+     var lat = cityWeather.city.coord.lat;
+     var lon = cityWeather.city.coord.lon;
+
+     $.ajax({
           url: forecastUrl,
           method: "GET"
      }).then(function(forecastResponse) {
           console.log(forecastResponse);
-          $("#fiveDayForecast").empty();
+          
 
-          for (var i = 1; i <= 5; i++) {
-               var day;
-               var temp;
-               var icon;
-               var wind;
-               var humidity;
-
-               day = forecastResponse.list[i].dt;
-               day = dayjs.unix(day).format("MM/DD/YYYY")
-
-
-               var dateInfo = { 
-                    
-                    date: forecastResponse.list[i].dt,
+          for (let i = 1; i < 5; i++) {
+               var cityObject = {
+                    day: forecastResponse.list[i].dt,
                     forecastIcon: forecastResponse.list[i].weather[0].icon,
-                    temperature: forecastResponse.list.main.temp,
-                    humidity: forecastResponse.list.main.humidity
-                    
-               }
+                    temp: forecastResponse.list[i].main.temp,
+                    humidity: forecastResponse.list[i].main.humidity,
+                    wind: forecastResponse.list[i].wind.speed,
+               };
+
+               var forecastDate = dayjs.unix(day).format("MM/DD/YYYY");
+               var forecastIconUrl = `<img src="https://openweathermap.org/img/wm/${cityObject.forecastIcon}@2x.png" />`
+
+               
+
+
+              var day = forecastResponse.list[i].dt;
+               var forecastDate = dayjs.unix(day).format("MM/DD/YYYY");
+               var temp = forecastResponse.list[i].main.temp;
+               var forecastIcon = forecastResponse.list[i].weather[0].icon;              
+               var wind = forecastResponse.list[i].wind.speed;;
+               var humidity = forecastResponse.list[i].main.humidity;
+               var forecastEl = $("#fiveDayForecast")
+               var lat = cityWeather.coord.lat;
+               var lon = cityWeather.coord.lon;
+          
+
+               var forecastCard = document.createElement('div');
+               forecastCard.classList.add("card", "col-sm-2", "bg-primary", "text-white");
+               
+               var dayCards = document.createElement("div");
+               dayCards.classList.add("card-body");
+               dayCards.innerHTML = `<h5>${day}</h5>
+                                   <img src="https://openweather.org/img/wn/${forecastIcon}@2x.png"> 
+                                   <br>
+                                   ${temp}
+                                   <br>
+                                   ${wind}
+                                   <br>
+                                   ${humidity}`
+
+               forecastCard.appendChild(dayCards);
+               forecastEl.append(forecastCard); 
+
+        
+
+              
+
+               
 
           }
-     })
+     });
      
-
+}
 
 $("#searchBtn").on("click", function(event) {
      event.preventDefault();
 
+     var search = $("#cityName").val().trim();
+          currentWeather(search);
+          if(!searchHistory.includes(search)) {
+               searchHistory.push(search);
+               var previousSearch = $(`
+                    <li class="list-group-item">${search}</li>
+                    `);
+               $("#pastSearch").append(previousSearch);
+          };
+          localStorage.setItem('search', JSON.stringify(searchHistory));
+          console.log(searchHistory);
 
 })
-}
